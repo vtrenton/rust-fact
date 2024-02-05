bca8afefeda9c5fbde939a39e8437938eeab04d0
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-02-05 12:35:07 -0600

    fixed some error handleing to avoid side-effects
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::time::Instant;
 
 fn main() {
     // 20! seems to be the max for u64
-    let input = 1;
+    let input = 20;
 
     if input < 1 {
         println!("Please enter an input greater than one");
@@ -12,53 +12,59 @@ fn main() {
     // calculate start time of the interative approach
     let iterstart = Instant::now();
     // perform the standard iterative approach
-    println!("The factorial of {} is {}", &input, iterfactorial(&input));
+    if let Ok(itfact) = iterfactorial(&input) {
+        println!("The factorial of {} is {}", &input, itfact);
+    } else {
+        // I want the program to exit here
+        // If we reach this point the rest of the program is broken
+        panic!("A really, really, really big number");
+    }
     // gather post execution time
     let itercost = iterstart.elapsed();
-    println!("Iterative approach: {}", itercost.subsec_nanos());
+    println!("Iterative approach: {} nanoseconds", itercost.subsec_nanos());
 
     // calculate start time of the functional approach
     let funcstart = Instant::now();
     // perform the functional approach
     let factor: u64 = 1;
-    println!("The factorial of {} is {}", &input, funcfactorial(&input, factor));
+    if let Ok(funfact) =  funcfactorial(&input, factor) {
+        println!("The factorial of {} is {}", &input, funfact);
+    } else {
+        // I cant think of a single reason we'd ever reach this
+        panic!("Something went VERY wrong");
+    }
     // gather post execution time
     let funccost = funcstart.elapsed();
-    println!("Functional approach: {}", funccost.subsec_nanos());
+    println!("Functional approach: {} nanoseconds", funccost.subsec_nanos());
 }
 
-fn iterfactorial(limit: &u64) -> u64 {
+fn iterfactorial(limit: &u64) -> Result<u64, String> {
     let mut factor: u64 = 1;
     for i in (1..=*limit).rev() {
         // use checked_mul to assure we don't overflow
-        match factor.checked_mul(i) {
-            Some(result) => factor = result,
-            None => {
-                // catch integer overflow and exit cleanly
-                panic!("A really, really, really big number");
-            }
-        }
+        factor = factor
+            .checked_mul(i)
+            .ok_or_else(|| "Integer Overflow".to_string())?;
     }
-    return factor;
+
+    Ok(factor)
 }
 
-fn funcfactorial(limit: &u64, mut result: u64) -> u64 {
+fn funcfactorial(limit: &u64, mut result: u64) -> Result<u64, String> {
     if *limit > 0 {
         let result = factmultiplier(&limit, &mut result);
         let iter = limit - 1;
-        funcfactorial(&iter, result)
+        funcfactorial(&iter, result?)
     } else {
-        return result;
+        Ok(result)
     }
 }
 
-fn factmultiplier(iter: &u64, factorial: &mut u64) -> u64 {
-    match factorial.checked_mul(*iter) {
-        Some(result) => *factorial = result,
-        None => {
-            // catch integer overflow and exit cleanly
-            panic!("A really, really, really big number");
-        }
-    }
-    return *factorial;
+fn factmultiplier(iter: &u64, factorial: &mut u64) -> Result<u64, String> {
+    // the check here is redundant since we already checked with iterfactorial
+    // but since this is a speed test we want to make the two approaches
+    // as close as possible
+    Ok(factorial
+        .checked_mul(*iter)
+        .ok_or_else(|| "Integer Overflow".to_string())?)
 }
